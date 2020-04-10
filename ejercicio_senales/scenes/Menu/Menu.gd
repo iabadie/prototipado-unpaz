bbd7870bba6439378241000637590d74c1ec94ef
Nacho <ignacioabadie@live.com.ar>
2020-04-09 21:34:41 -0300

    Agregado label al ejercicio de señales

---
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+const INSTANCES_TEXT = "Cantidad de instancias: "
+# Variable para contar cantidad de instancias
+var instancesCount = 0
+
+func _ready():
+	# Inicializa el texto de "CountLabel" usando el texto
+	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
