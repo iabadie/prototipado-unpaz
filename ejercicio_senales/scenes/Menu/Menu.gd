67e054396145e08e344368f83d3b30a305eb518f
Ignacio Abadie <ignacioabadie@live.com.ar>
2023-05-07 21:54:12 -0300

    Actualización ejercicio de señales

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -7,5 +7,5 @@ var instancesCount = 0
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
-	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
